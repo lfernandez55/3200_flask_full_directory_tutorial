eaf6b1c060bbe41940f1fb92fd6cd544cc2d258a
lfernandez55 <lfernandez@weber.edu>
2020-02-05 22:11:24 -0700

    First commit

---
+++ app/__init__.py
@@ -0,0 +1,44 @@
+# __init__.py is a special Python file that allows a directory to become
+# a Python package so it can be accessed using the 'import' statement.
+
+from datetime import datetime
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# this is defined globally - so a little differently than how we used to do it
+# see how it's connected to the flask app below
+db = SQLAlchemy()
+
+# Initialize Flask Application
+def create_app(extra_config_settings={}):
+    """Create a Flask application.
+    """
+    # Instantiate Flask
+    app = Flask(__name__)
+
+    # Load common settings
+    app.config.from_object('app.settings')
+    # Load extra settings from extra_config_settings param
+    app.config.update(extra_config_settings)
+
+    # Setup Flask-SQLAlchemy
+    # up to now we did: db = SQLAlchemy(app) now we do:
+    db.init_app(app)
+
+    # Register blueprints
+    from .views import register_blueprints
+    register_blueprints(app)
+
+    # Setup Flask-User to handle user account related forms
+    # from .models.user_models import User
+
+    # declare your app.context processors here
+    # @app.context_processor
+
+
+
+    return app
+
+
---
+++ app/views/book_views.py
@@ -0,0 +1,17 @@
+# Copyright 2014 SolidBuilds.com. All rights reserved
+#
+# Authors: Ling Thio <ling.thio@gmail.com>
+
+
+from flask import Blueprint, redirect, render_template
+from flask import request, url_for
+
+
+from app import db
+
+book_blueprint = Blueprint('books', __name__, template_folder='templates')
+
+
+@book_blueprint.route('/foo')
+def foo():
+    return render_template('books/foo.html')
